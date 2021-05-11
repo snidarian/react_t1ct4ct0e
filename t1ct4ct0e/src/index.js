f1562fd4b3e3f094b10670dcd1dc81b8f9d244ee
snidarian <cephalopod31956@gmail.com>
2021-05-11 07:12:11 -0400

    rendering

--- t1ct4ct0e/src/index.js
+++ t1ct4ct0e/src/index.js
@@ -8,7 +8,7 @@ class Square extends React.Component {
     render() {
         return (
             <button className="square">
-                {/* TODO */}
+                {this.props.value}
             </button>
         );
     }
@@ -17,7 +17,7 @@ class Square extends React.Component {
 // board
 class Board extends React.Component {
     renderSquare(i) {
-        return <Square />;
+        return <Square value={i}/>;
     }
 
     render() {
