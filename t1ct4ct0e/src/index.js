5b02dfa77c23ed9c549978790bc73d8237c71806
snidarian <cephalopod31956@gmail.com>
2021-05-11 08:59:15 -0400

    nmrs chngs

--- t1ct4ct0e/src/index.js
+++ t1ct4ct0e/src/index.js
@@ -7,8 +7,11 @@ import ReactDom from 'react-dom'
 class Square extends React.Component {
     render() {
         return (
-            <button className="square">
-                {this.props.value}
+            <button 
+            className="square" 
+            onClick={() => this.props.onClick()}
+            >
+            {this.props.value}
             </button>
         );
     }
@@ -16,8 +19,19 @@ class Square extends React.Component {
 
 // board
 class Board extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            squares: Array(9).fill(null),
+        };
+    }
     renderSquare(i) {
-        return <Square value={i}/>;
+        return ( 
+        <Square 
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)} 
+        />
+        );
     }
 
     render() {
